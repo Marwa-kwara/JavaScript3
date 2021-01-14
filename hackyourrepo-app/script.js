db1fa0ce8bd4c34b8ce7193e4123de1875bd03b6
Marwa <makw90@hotmail.com>
2021-01-14 21:38:09 +0100

    no change

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -3,3 +3,24 @@
 /*
   Write here your JavaScript for HackYourRepo!
 */
+
+const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
+function fetchJson(url, callback){
+  const xhr = new XMLHttpRequest();
+  xhr.response = "json";
+
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 400){
+      callback (null, xhr.response)
+    }
+    else{
+      callback(newError(' ${xhr.status}'), null)
+    }
+  }
+  xhr.onerror = () => {
+    callback(newError('Network request failed', null))
+  }
+  xhr.open('GET', url);
+  xhr.send()
+}
