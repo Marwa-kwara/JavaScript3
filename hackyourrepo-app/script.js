0b0af3f04eacc1630633f2dba3ca1e4c3ae99c59
Marwa <makw90@hotmail.com>
2021-01-22 21:58:18 +0100

    add js

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -1,26 +1,153 @@
-"use strict";
-
 /*
   Write here your JavaScript for HackYourRepo!
 */
 
+function main (){
+const body = document.body;
+
+//Header
+const header = document.createElement('div');
+header.id ='header';
+body.appendChild(header)
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Hack Your Future Repositories';
+header.appendChild(h1);
+
+const select = document.createElement('select');
+select.innerText = 'Repositories ...'
+header.appendChild(select)
+
+//main 
+ const mainContainer = document.createElement('div');
+ mainContainer.id = 'main-container';
+ body.appendChild(mainContainer);
+
+ //create ul section inside the main
+ const infoSection = document.createElement('div');
+ infoSection.className = 'sec';
+ mainContainer.appendChild(infoSection);
+ const list = document.createElement('ul');
+
+ const repo = document.createElement('li');
+ repo.innerText = 'Name : ';
+ const repoName = document.createElement('p');
+ repo.appendChild(repoName);
+
+const description = document.createElement('li');
+description.innerText = 'Description : '
+const repoDescription = document.createElement('p');
+description.appendChild(repoDescription);
+
+const forks = document.createElement('li');
+forks.innerText = 'Forks : ';
+const repoForks = document.createElement('p');
+forks.appendChild(repoForks);
+
+const updated = document.createElement('li');
+updated.innerText = 'Updated : '
+const repoUpdated = document.createElement('p');
+updated.appendChild(repoUpdated)
+
+list.append(repo, description, forks, updated);
+infoSection.appendChild(list);
+
+
+// create the contributor section inside the main
+const contributorsSection = document.createElement('div');
+contributorsSection.className = 'sec';
+mainContainer.appendChild(contributorsSection);
+
+const h3 = document.createElement('h3');
+h3.innerText = 'Contributors';
+contributorsSection.appendChild(h3);
+
+/******************************************************************** */
+
 const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
-function fetchJson(url, callback){
-  const xhr = new XMLHttpRequest();
-  xhr.response = "json";
+//show more  repo details .
+let repoInfo;
 
-  xhr.onload = () => {
-    if (xhr.status >= 200 && xhr.status < 400){
-      callback (null, xhr.response)
-    }
-    else{
-      callback(newError(' ${xhr.status}'), null)
-    }
-  }
-  xhr.onerror = () => {
-    callback(newError('Network request failed', null))
-  }
-  xhr.open('GET', url);
-  xhr.send()
+function fetchData() {
+  fetch(url)
+  .then(response => {
+     return response.json();
+   })
+   .then(data => {
+      console.log(data)
+      repoInfo = data;
+      selectOption(repoInfo);
+   })
+   .catch(error => {
+     console.log('err', error);
+   });
 }
+
+function selectOption (repoInfo){
+repoInfo.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
+ for ( let i = 0 ;i < repoInfo.length; i++){
+  const option = document.createElement('option')
+  option.value = repoInfo[i].name;
+  option.innerText = repoInfo[i].name;
+  select.appendChild(option)
+} 
+}
+
+select.addEventListener('change',()=>{
+  const repo = repoInfo.find(repo => {
+    return repo.name === select.value;
+  });
+  if (!repo) {
+    throw new Error("weird, this should not happen");
+  }
+  repoName.innerText = repo.name;
+  repoDescription.innerText = repo.description;
+  repoForks.innerText = repo.forks;
+  repoUpdated.innerText = repo.updated_at
+});
+
+fetchData()
+
+// fetch the contributors
+function fetchContributors (){
+  const contributorsUrl = repoInfo[value].contributors_url;
+
+  fetch(contributorsUrl)
+  .then(response => {
+     return response.json();
+   })
+   .then(data => {
+       console.log(data);
+       selectContributors();
+   })
+   .catch(error => {
+     console.log('err', error);
+   });
+
+function selectContributors (){
+  repoInfo.forEach(element => {
+    const contributorCard = document.createElement('div');
+
+    const contributorImage = document.createElement('img');
+    contributorImage.src = contributor.avatar_url;
+
+    const contributorName = document.createElement('a');
+    contributorName.innerText = contributor.login;
+    contributorName.href = contributor.html_url;
+
+    const contributions = document.createElement('div');
+    contributions.innerText = contributor.contributions;
+
+    contributorCard.append(contributorImage,contributorName,contributions);
+    contributorsSection.appendChild(contributorCard)
+  });
+
+}
+}
+fetchContributors()
+
+}
+
+window.addEventListener("load", main)
